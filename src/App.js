dc1cbb5f31bd166383bc93d083cdfeca69ccf7b2
therealsqvad <nikita_klepov@mail.ru>
2019-06-24 23:14:32 +0700

    refactor code

--- src/App.js
+++ src/App.js
@@ -37,19 +37,19 @@ class App extends Component {
       url: 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fio'
     };
 
-    axios(options).then(response => {
-      // console.log(response);
-      const names = response.data.suggestions.map(i => ({ name: i.value }));
+    axios(options)
+      .then(response => {
+        const names = response.data.suggestions.map(i => ({ name: i.value }));
 
-      this.setState({
-        isLoading: false,
-        names
-      });
-    })
+        this.setState({
+          isLoading: false,
+          names
+        });
+      })
       .catch(error => {
         console.log(error);
       });
-  }
+  };
 
   getSurname = surname => {
     this.setState({ isLoading: true });
@@ -65,19 +65,19 @@ class App extends Component {
       url: 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fio'
     };
 
-    axios(options).then(response => {
-      // console.log(response);
-      const surnames = response.data.suggestions.map(i => ({ surname: i.value }));
+    axios(options)
+      .then(response => {
+        const surnames = response.data.suggestions.map(i => ({ surname: i.value }));
 
-      this.setState({
-        isLoading: false,
-        surnames
-      });
-    })
+        this.setState({
+          isLoading: false,
+          surnames
+        });
+      })
       .catch(error => {
         console.log(error);
       });
-  }
+  };
 
   getPatronymic = patronymic => {
     this.setState({ isLoading: true });
@@ -93,19 +93,19 @@ class App extends Component {
       url: 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fio'
     };
 
-    axios(options).then(response => {
-      // console.log(response);
-      const patronymics = response.data.suggestions.map(i => ({ patronymic: i.value }));
+    axios(options)
+      .then(response => {
+        const patronymics = response.data.suggestions.map(i => ({ patronymic: i.value }));
 
-      this.setState({
-        isLoading: false,
-        patronymics
-      });
-    })
+        this.setState({
+          isLoading: false,
+          patronymics
+        });
+      })
       .catch(error => {
         console.log(error);
       });
-  }
+  };
 
   getFIO = () => {
     const { surname, name, patronymic } = this.state;
@@ -121,134 +121,132 @@ class App extends Component {
       url: 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/party'
     };
 
-    axios(options).then(response => {
-      console.log(response);
-      if (response.data.suggestions.length > 0) {
-        this.setState({
-          qc: false
-        });
-      } else {
-        this.setState({ show: true, qc: true });
-      }
-    })
+    axios(options)
+      .then(response => {
+        console.log(response);
+        if (response.data.suggestions.length > 0) {
+          this.setState({
+            qc: false
+          });
+        } else {
+          this.setState({ show: true, qc: true });
+        }
+      })
       .catch(error => {
         this.setState({ show: true });
         console.log(error);
       });
-  }
+  };
 
   handleShow = () => {
     this.setState({ show: true });
+  };
+
+  handleClose = () => this.setState({ show: false });
+
+  render() {
+    const { surnames, names, patronymics } = this.state;
+    const {
+      selectSurname, selectName, selectPatronymic, qc
+    } = this.state;
+    const { show } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Form>
+            <Form.Label>Введите ФИО</Form.Label>
+            <Form.Row>
+              <Col>
+                <AsyncTypeahead
+                  {...this.state}
+                  id="surname"
+                  labelKey="surname"
+                  minLength={2}
+                  onSearch={this.getSurname}
+                  onChange={selected => {
+                    this.setState({ selectSurname: selected });
+                    if (selected.length > 0) {
+                      this.setState({ surname: selected[0].surname });
+                    }
+                  }}
+                  placeholder="Фамилия"
+                  options={surnames}
+                  selected={selectSurname}
+                />
+              </Col>
+              <Col>
+                <AsyncTypeahead
+                  {...this.state}
+                  id="name"
+                  labelKey="name"
+                  minLength={2}
+                  onSearch={this.getName}
+                  onChange={selected => {
+                    this.setState({ selectName: selected });
+                    if (selected.length > 0) {
+                      this.setState({ name: selected[0].name });
+                    }
+                  }}
+                  placeholder="Имя"
+                  options={names}
+                  selected={selectName}
+                />
+              </Col>
+              <Col>
+                <AsyncTypeahead
+                  {...this.state}
+                  id="patronymic"
+                  labelKey="patronymic"
+                  minLength={2}
+                  onSearch={this.getPatronymic}
+                  onChange={selected => {
+                    this.setState({ selectPatronymic: selected });
+                    if (selected.length > 0) {
+                      this.setState({ patronymic: selected[0].patronymic });
+                    }
+                  }}
+                  placeholder="Отчество"
+                  options={patronymics}
+                  selected={selectPatronymic}
+                />
+              </Col>
+            </Form.Row>
+            <Button variant="primary" type="button" onClick={this.getFIO}>
+              Поиск
+            </Button>
+            <Form.Row>
+              <Col />
+              <Col>
+                <Toast onClose={this.handleClose} show={show} delay={3000} autohide>
+                  <Toast.Header>
+                    <strong className="mr-auto">Ошибка</strong>
+                  </Toast.Header>
+                  <Toast.Body className="toast-body">
+                    <p>По данному запросу ничего не найдено</p>
+                  </Toast.Body>
+                </Toast>
+              </Col>
+              <Col />
+            </Form.Row>
+          </Form>
+          <Form hidden={qc}>
+            <Form.Row>
+              <Col>
+                <Form.Control size="lg" placeholder="Название" />
+              </Col>
+              <Col>
+                <Form.Control size="lg" placeholder="ИНН" />
+              </Col>
+              <Col>
+                <Form.Control size="lg" placeholder="Адрес" />
+              </Col>
+            </Form.Row>
+          </Form>
+        </header>
+      </div>
+    );
   }
-
- handleClose = () => this.setState({ show: false });
-
-
- render() {
-   // const data = `${surname} ${name} ${patronymic}`;
-
-   const { surnames, names, patronymics } = this.state;
-   const {
-     selectSurname, selectName, selectPatronymic, qc
-   } = this.state;
-   const { show } = this.state;
-
-   return (
-     <div className="App">
-       <header className="App-header">
-         <Form>
-           <Form.Label>Введите ФИО</Form.Label>
-           <Form.Row>
-             <Col>
-               <AsyncTypeahead
-                 {...this.state}
-                 id="surname"
-                 labelKey="surname"
-                 minLength={2}
-                 onSearch={this.getSurname}
-                 onChange={selected => {
-                   this.setState({ selectSurname: selected });
-                   if (selected.length > 0) {
-                     this.setState({ surname: selected[0].surname });
-                   }
-                 }}
-                 placeholder="Фамилия"
-                 options={surnames}
-                 selected={selectSurname}
-               />
-             </Col>
-             <Col>
-               <AsyncTypeahead
-                 {...this.state}
-                 id="name"
-                 labelKey="name"
-                 minLength={2}
-                 onSearch={this.getName}
-                 onChange={selected => {
-                   this.setState({ selectName: selected });
-                   if (selected.length > 0) {
-                     this.setState({ name: selected[0].name });
-                   }
-                 }}
-                 placeholder="Имя"
-                 options={names}
-                 selected={selectName}
-               />
-             </Col>
-             <Col>
-               <AsyncTypeahead
-                 {...this.state}
-                 id="patronymic"
-                 labelKey="patronymic"
-                 minLength={2}
-                 onSearch={this.getPatronymic}
-                 onChange={selected => {
-                   this.setState({ selectPatronymic: selected });
-                   if (selected.length > 0) {
-                     this.setState({ patronymic: selected[0].patronymic });
-                   }
-                 }}
-                 placeholder="Отчество"
-                 options={patronymics}
-                 selected={selectPatronymic}
-               />
-             </Col>
-           </Form.Row>
-           <Button variant="primary" type="button" onClick={this.getFIO}>
-            Поиск
-           </Button>
-           <Form.Row>
-             <Col />
-             <Col>
-               <Toast onClose={this.handleClose} show={show} delay={3000} autohide>
-                 <Toast.Header>
-                   <strong className="mr-auto">Ошибка</strong>
-                 </Toast.Header>
-                 <Toast.Body className="toast-body">
-                   <p>По данному запросу ничего не найдено</p>
-                 </Toast.Body>
-               </Toast>
-             </Col>
-             <Col />
-           </Form.Row>
-         </Form>
-         <Form hidden={qc}>
-           <Form.Row>
-             <Col>
-               <Form.Control size="lg" placeholder="Название" />
-             </Col>
-             <Col>
-               <Form.Control size="lg" placeholder="ИНН" />
-             </Col>
-             <Col>
-               <Form.Control size="lg" placeholder="Адрес" />
-             </Col>
-           </Form.Row>
-         </Form>
-       </header>
-     </div>
-   );
- }
 }
 
 export default App;
